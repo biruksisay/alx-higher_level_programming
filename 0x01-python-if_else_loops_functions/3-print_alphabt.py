6e6143de2ad4023952569456a9aa52e26c5d152d
biruksisay <biruksisay70@gmail.com>
2022-08-23 06:31:24 -0700

    3-print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for sletter in range(97, 123):
-    if chr(sletter) is not 'q' and chr(sletter) is not 'e':
-        print("{}".format(chr(sletter)), end="")
+
+for x in range(ord("a"), ord("z") + 1):
+    if chr(x) != 'q' and chr(x) != 'e':
+        print("{}".format(chr(x)), end="")
