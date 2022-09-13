cecff9438cedde54de2084dd3d07229372559465
biruksisay <biruksisay70@gmail.com>
2022-09-13 04:32:23 -0700

    fixing

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,81 +1,53 @@
 #!/usr/bin/python3
+"""Coordinates of a square"""
 
 
 class Square:
-    """Square class with validated private instance attribute size"""
+    """Private instance attribute: size
+    Instantiation with area and position method """
 
     def __init__(self, size=0):
-        """Args:
-               size: size of square
-        """
-        if type(size) is not int:
-            raise TypeError("size must be a number")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        """Initializes attribute size """
+        self.size = size
 
-    def __eq__(self, other):
-        """Checks if two Squares are equal
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size == other.size
-
-    def __ne__(self, other):
-        """Checks if two Squares are not equal
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size != other.size
-
-    def __gt__(self, other):
-        """Checks if current Square is greater than other Square
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size > other.size
-
-    def __ge__(self, other):
-        """Checks if current Square is greater than or equal to other Square
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size >= other.size
-
-    def __lt__(self, other):
-        """Checks if current Square is less than other Square
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size < other.size
-
-    def __le__(self, other):
-        """Checks if current Square is less than or equal to other Square
-        Args:
-            other: Square to compare to current instance
-        """
-        return self.size <= other.size
+    def area(self):
+        """Calculate area of square"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """size: size of square
-        setter validates size is an integer and >= 0
-        Raises:
-            TypeError: If size is not an int
-            ValueError: If size is < 0
-        """
+        """Getter for square"""
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) is not int:
-            raise TypeError("size must be a number")
-        elif size < 0:
+    def size(self, value):
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = value
 
-    def area(self):
-        """Calculates the area of Square instance and returns it"""
-        return self.size ** 2
+    def __eq__(self, other):
+        """Equal"""
+        return self.size == other.size
+
+    def __ne__(self, other):
+        """Not Equal"""
+        return self.size != other.size
+
+    def __lt__(self, other):
+        """Less than"""
+        return self.size < other.size
+
+    def __le__(self, other):
+        """Less than or equal"""
+        return self.size <= other.size
+
+    def __gt__(self, other):
+        """Greater than"""
+        return self.size > other.size
+
+    def __ge__(self, other):
+        """Greater than or equal"""
+        return self.size >= other.size
