2a14e05c7fb8d992b61ba1b85da7600746f1cb00
biruksisay <biruksisay70@gmail.com>
2022-09-13 04:16:25 -0700

    fixing

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,76 +1,108 @@
 #!/usr/bin/python3
+"""
+Singly linked list Module
+"""
 
 
 class Node:
-    """Node class is a node of a singly linked list"""
-
+    """
+    Node class
+    Attributes:
+        data (int): the data stored in the node
+        next_node (Node): a pointer to the next node
+            in the linked list
+    """
     def __init__(self, data, next_node=None):
-        """Args:
-                data: value of node
-                next_node: next Node instance
+        """
+        Class initializer
+        Args:
+            data (int): the data stored in the node.
+            next_node (Node): a pointer to the next node
         """
         self.data = data
-        self.next_node = next_node
+        self.next_node = None
 
     @property
     def data(self):
-        """data: value of node
-        setter validates value is type int
-        Raises:
-            TypeError: If data is not type int
+        """
+        Getter of the attribute data
+        Returns:
+            data: the stored data
         """
         return self.__data
 
     @data.setter
     def data(self, value):
-        if isinstance(value, int):
-            self.__data = value
+        """
+        Setter of the attribure data
+        Args:
+            value (int): the given data
+        Raises:
+            TypeError: if value is not integer
+        """
+        if not type(value) is int:
+            raise TypeError("data must be integer")
         else:
-            raise TypeError("data must be an integer")
+            self.__data = value
 
     @property
     def next_node(self):
-        """next_node: Node instance of next node in list
-        setter validates next_node is valid
-        Raises:
-            TypeError: If next_node is not a Node instance or None
+        """
+        Getter of the next_node
+        Returns:
+            next_node: a pointer to the next node
         """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if value is None or isinstance(value, Node):
-            self.__next_node = value
-        else:
+        """
+        Setter of the next_node
+        Args:
+            value (Node): a pointer to the next node
+        Raises:
+            TypeError: if value not node or None
+        """
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
-
+        else:
+            self.__next_node = value
 
 class SinglyLinkedList:
-    """SinglyLinkedList class is a singly linked list"""
-
+    """
+    SiglyLinkedList defines the singly linked list
+    Attributes:
+        head (Node): a pointer to the singly linked list
+    """
     def __init__(self):
-        """Initialize private instance variable to None (empty list)"""
+        """
+        Class initializer
+        """
         self.__head = None
 
     def __str__(self):
-        """Returns string version of singly linked list"""
-        if self.__head is None:
-            return ""
-        temp = self.__head
-        list_str = ""
-        while temp is not None:
-            list_str += str(temp.data)
-            temp = temp.next_node
-            if temp is not None:
-                list_str += "\n"
-        return list_str
+        result = ""
+        tmp = self.__head
+        while tmp is not None:
+            result += str(self.data)
+            result += '\n'
+            tmp = tmp.__next_node
+        return result
 
     def sorted_insert(self, value):
-        """Insert new Node to correct sorted position (increasing order)"""
-        if self.__head is None or value < self.__head.data:
-            self.__head = Node(value, self.__head)
-            return
-        temp = self.__head
-        while temp.next_node is not None and temp.next_node.data < value:
-            temp = temp.next_node
-        temp.next_node = Node(value, temp.next_node)
+        """
+        Inserts new Node into the correct soeted position
+        """
+        if self.__head is None:
+            self.__head = Node(value)
+        else:
+            new_node = Node(value)
+            tmp = self.__head
+            while tmp is not None:
+                if tmp.__next_node is None:
+                    tmp.__next_node = new_node
+                    new_node.__next_node = None
+                if new_node.__data < tmp.__next_node.__data:
+                    new_node.__next_node = tmp.__next_node
+                    tmp.__next_node = new_node
+                tmp = tmp.__next_node
