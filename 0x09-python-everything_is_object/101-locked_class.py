da637ccc1cafa17aa3996a8d91263fd4df4f3be0
biruksisay <biruksisay70@gmail.com>
2022-09-20 06:26:16 -0700

    fixing

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-LockedClass
-"""
+    Locked Class Mdule
+    """
 
 
 class LockedClass:
-    """ No class or object attributes, can't set
-        Except for first_name
     """
-    def __setattr__(self, attribute, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+    prevents the user from dynamically creating new instance attributes,
+    except if the
+    new instance attribute is called first_name.
+    """
+    __slots__ = 'first_name'
