99eb71b3692c3ce713fc0a26dc8eda7164a727ab
biruksisay <biruksisay70@gmail.com>
2022-09-22 05:10:03 -0700

    fixing errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,39 @@
-#!/usr/bin/python3
-"""Module to find the max integer in a list
+#!/usr/bin/python
+"""
+tests for 6-max_integer
 """
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ tests for max_integer"""
+    def test_def(self):
+        """tests normal input"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_none(self):
+        """tests empty input"""
+        self.assertEqual(max_integer(), None)
+
+    def test_neg(self):
+        """tests negative"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_neg_pos(self):
+        """tests composed input"""
+        self.assertEqual(max_integer([1, -4, -5, 5, 3]), 5)
+
+    def test_non_int(self):
+        """tests with non integer input"""
+        with self.assertRaises(TypeError):
+            max_integer([1, '2', 3])
+
+    def test_float(self):
+        """tests with float"""
+        self.assertEqual(max_integer([1, 2, 3.3]), 3.3)
+
+if __name__ == "__main__":
+    unittest.main()
