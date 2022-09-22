c41d5dc00b94f14210701ba4ff4e260166a85eca
biruksisay <biruksisay70@gmail.com>
2022-09-22 05:29:08 -0700

    fixing

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """
-Module print_square
-print_square
+This is the print_square module.
+This module supplies one function, print_square().
 """
 
 
 def print_square(size):
     """
-    Print a square
+    Print a square with the character #.
+    Args:
+        size (int): the size length of the square.
     """
-    if type(size) != int:
-        raise TypeError('size must be an integer')
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be a non-negative integer')
-    if size == 0:
-        return
-    for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+        raise ValueError("size must be >= 0")
+
+    print(("#" * size + '\n') * size, end='')
