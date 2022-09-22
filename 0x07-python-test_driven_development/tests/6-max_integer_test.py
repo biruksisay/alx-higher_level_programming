e5cef80e0a467fd14477dc9ed41c965c6fc588d2
biruksisay <biruksisay70@gmail.com>
2022-09-22 05:18:21 -0700

    fixing

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,39 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""Max integer test module.
+This module contains a class that tests for the max_integer function.
 """
-tests for 6-max_integer
-"""
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ tests for max_integer"""
-    def test_def(self):
-        """tests normal input"""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    """Test function"""
 
-    def test_none(self):
-        """tests empty input"""
-        self.assertEqual(max_integer(), None)
+    def test_max_integer(self):
+        """
+        Tests if correct tests are ok
+        """
+        lst = [1, 2, 3, 4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [1, 4, 2, 3, -4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [None]
+        self.assertEqual(max_integer(lst), None)
+        lst = []
+        self.assertEqual(max_integer(lst), None)
+        lst = [1, 3, float('inf'), 2]
+        self.assertEqual(max_integer(lst), float('inf'))
+        lst = [1, 3, -float('inf'), 2]
+        self.assertEqual(max_integer(lst), 3)
+        lst = [4, 1, 2, 3]
+        self.assertEqual(max_integer(lst), 4)
 
-    def test_neg(self):
-        """tests negative"""
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-
-    def test_neg_pos(self):
-        """tests composed input"""
-        self.assertEqual(max_integer([1, -4, -5, 5, 3]), 5)
-
-    def test_non_int(self):
-        """tests with non integer input"""
-        with self.assertRaises(TypeError):
-            max_integer([1, '2', 3])
-
-    def test_float(self):
-        """tests with float"""
-        self.assertEqual(max_integer([1, 2, 3.3]), 3.3)
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_type(self):
+        """
+        Tests if failure tests are ok
+        """
+        lst = [1, 2, 3, "Betty"]
+        self.assertRaises(TypeError, max_integer, lst)
+        lst = None
+        self.assertRaises(TypeError, max_integer, lst)
