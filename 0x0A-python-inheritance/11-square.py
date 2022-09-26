a771abef4f8dce21d7711c56ab02671a924c2f31
biruksisay <biruksisay70@gmail.com>
2022-09-26 02:23:47 -0700

    fixing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+"""
+    Square Module
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class module.
-Define Square class.
-"""
-
-
 class Square(Rectangle):
-    """Define a Square."""
-
+    """
+        Square Class
+    """
     def __init__(self, size):
-        super().integer_validator("size", size)
+        """
+            Initialize the square base on Rectangle
+        """
+        self.integer_validator('size', size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        """Returns the area of the square."""
-        return self.__size * self.__size
+        """
+        define area of the square
+        """
+        return self.__size ** 2
 
     def __str__(self):
-        return "[{}] {}/{}".format(type(self).__name__, self.__size,
-                                   self.__size)
+        """
+            __str__
+        """
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
