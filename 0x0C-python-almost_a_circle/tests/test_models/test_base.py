debedf4ec713cacecac7dc8b70f6d0dbda51a774
biruksisay <biruksisay70@gmail.com>
2022-10-07 01:17:45 -0700

    fixing_models

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,84 +1,73 @@
 #!/usr/bin/python3
 """
-Contains tests for Base class
+Unittest for base module.
+Test cases for base class.
 """
 
 import unittest
-import json
-from models import base
-Base = base.Base
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """check functionality of Base class"""
-    def _too_many_args(self):
-        """testing too many args to init"""
-        with self.assertRaises(TypeError):
-            b = Base(1, 1)
+class TestBaseClass(unittest.TestCase):
+    """A test class created to run tests for Base class"""
 
-    def _no_id(self):
-        """Testing id as None"""
-        b = Base()
-        self.assertEqual(b.id, 1)
-
-    def _id_set(self):
-        """Testing id as not None"""
-        b98 = Base(98)
-        self.assertEqual(b98.id, 98)
-
-    def _no_id_after_set(self):
-        """Testing id as None after not None"""
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-
-    def _nb_private(self):
-        """Testing nb_objects as a private instance attribute"""
-        b = Base(3)
-        with self.assertRaises(AttributeError):
-            print(b.nb_objects)
-        with self.assertRaises(AttributeError):
-            print(b.__nb_objects)
-
-    def _to_json_string(self):
-        """Testing regular to json string"""
+    def setUp(self):
         Base._Base__nb_objects = 0
-        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
-        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
-        json_s = Base.to_json_string([d1, d2])
-        self.assertTrue(type(json_s) is str)
-        d = json.loads(json_s)
-        self.assertEqual(d, [d1, d2])
 
-    def _empty_to_json_string(self):
-        """Test for passing empty list"""
-        json_s = Base.to_json_string([])
-        self.assertTrue(type(json_s) is str)
-        self.assertEqual(json_s, "[]")
+    def test_0_id_None(self):
+        '''Test for id with None argument passed'''
+        b1 = Base(None)
+        self.assertEqual(b1.id, 1)
 
-    def _None_to_json_String(self):
-        """testting None to a json"""
-        json_s = Base.to_json_string(None)
-        self.assertTrue(type(json_s) is str)
-        self.assertEqual(json_s, "[]")
+    def test_1_id(self):
+        '''Test to check for id'''
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(11)
+        b4 = Base(-12)
+        b5 = Base(0)
+        b6 = Base(1.1)
+        b7 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 11)
+        self.assertEqual(b4.id, -12)
+        self.assertEqual(b5.id, 0)
+        self.assertEqual(b6.id, 1.1)
+        self.assertEqual(b7.id, 3)
 
-    def _from_json_string(self):
-        """Tests normal from_json_string"""
-        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
-{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
-        json_l = Base.from_json_string(json_str)
-        self.assertTrue(type(json_l) is list)
-        self.assertEqual(len(json_l), 2)
-        self.assertTrue(type(json_l[0]) is dict)
-        self.assertTrue(type(json_l[1]) is dict)
-        self.assertEqual(json_l[0],
-                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
-        self.assertEqual(json_l[1],
-                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+    def test_2_id_single(self):
+        '''Test for single instance creation with id'''
+        b1 = Base(3)
+        self.assertEqual(b1.id, 3)
 
-    def _frjs_empty(self):
-        """Tests from_json_string  empty string"""
-        self.assertEqual([], Base.from_json_string(""))
+    def test_3_id_multi(self):
+        '''Test for multiple instance creation with id'''
+        b1 = Base(3)
+        self.assertEqual(b1.id, 3)
+        b2 = Base(4)
+        self.assertEqual(b2.id, 4)
 
-    def _frjs_None(self):
-        """Testing from_json_string   none string"""
-        self.assertEqual([], Base.from_json_string(None))
+    def test_4_id_string(self):
+        '''Test for string argument'''
+        b1 = Base("foo")
+        self.assertEqual(b1.id, "foo")
+
+    def test_5_id_NaN(self):
+        '''Test for NaN as argument'''
+        b1 = Base(float("nan"))
+        self.assertNotEqual(b1.id, float("nan"))
+
+    def test_6_id_sameId(self):
+        '''Test for same ids'''
+        b1 = Base(22)
+        self.assertEqual(b1.id, 22)
+        b2 = Base(22)
+        self.assertEqual(b2.id, 22)
+
+
+if __name__ == '__main__':
+    unittest.main()
